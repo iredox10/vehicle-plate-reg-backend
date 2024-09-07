537625afd2218388c2ffabe0df6aff911c110fb3
iredox <idreesadam200@gmail.com>
2024-09-07 12:09:46 +0100

    comment local mongoose

--- config/mongooseConnect.js
+++ config/mongooseConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 const mongooseConnect = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URI)
+        // await mongoose.connect(process.env.MONGODB_URI)
         await mongoose.connect('mongodb+srv://idreesadam200:iredox@cluster0.jcxb8on.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log('mongoose connected')
     }catch(err){
